9191a5315f731c04a326a1f6b0f17245fb5bbfc0
MerchantPug <merchantpug@gmail.com>
2025-02-16 12:11:54 +1100

    Update ModDevGradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     id("fabric-loom") version "1.8-SNAPSHOT" apply false
-    id("net.neoforged.moddev") version "2.0.44-beta" apply false
+    id("net.neoforged.moddev") version "2.0.78" apply false
     id("me.modmuss50.mod-publish-plugin") version "0.8.0" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
