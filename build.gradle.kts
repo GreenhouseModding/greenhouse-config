7de1bed51b61413814a92294372ce7764b7ce776
MerchantPug <merchantpug@gmail.com>
2025-02-16 12:14:20 +1100

    Update Mod Publish Plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("fabric-loom") version "1.8-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.78" apply false
-    id("me.modmuss50.mod-publish-plugin") version "0.8.0" apply false
+    id("me.modmuss50.mod-publish-plugin") version "0.8.4" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
