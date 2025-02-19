be69baebbb70b30e3ace990ab00a8a7dfb949e81
MerchantPug <merchantpug@gmail.com>
2025-02-20 02:13:29 +1100

    Fix client config initialization crashes due to Fabric random load order.

--- core/fabric/src/main/java/house/greenhouse/greenhouseconfig/impl/client/GreenhouseConfigFabricClient.java
+++ core/fabric/src/main/java/house/greenhouse/greenhouseconfig/impl/client/GreenhouseConfigFabricClient.java
@@ -1,15 +1,22 @@
 package house.greenhouse.greenhouseconfig.impl.client;
 
+import house.greenhouse.greenhouseconfig.impl.GreenhouseConfig;
 import house.greenhouse.greenhouseconfig.impl.network.SyncGreenhouseConfigPacket;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.event.Event;
+import net.minecraft.resources.ResourceLocation;
 
 public class GreenhouseConfigFabricClient implements ClientModInitializer {
+    public static final ResourceLocation CONFIG_INITIALIZATION_PHASE = GreenhouseConfig.asResource("config_initialization");
+
     @Override
     public void onInitializeClient() {
-        GreenhouseConfigClient.init();
+        ClientLifecycleEvents.CLIENT_STARTED.addPhaseOrdering(Event.DEFAULT_PHASE, CONFIG_INITIALIZATION_PHASE);
+        ClientLifecycleEvents.CLIENT_STARTED.register(CONFIG_INITIALIZATION_PHASE, client -> GreenhouseConfigClient.init());
 
         ClientConfigurationNetworking.registerGlobalReceiver(SyncGreenhouseConfigPacket.TYPE, (payload, context) -> payload.handleConfiguration());
         ClientPlayNetworking.registerGlobalReceiver(SyncGreenhouseConfigPacket.TYPE, (payload, context) -> payload.handlePlay());
