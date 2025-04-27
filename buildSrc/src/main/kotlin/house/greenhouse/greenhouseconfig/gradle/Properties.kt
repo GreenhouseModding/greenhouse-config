9508f2a1a02b36c196406fe8f31233c5fdceccad
Anastasia Calico <calico@merchantcalico.net>
2025-04-27 15:26:21 +1000

    chore: Update extra versions and CHANGELOG.md.

--- buildSrc/src/main/kotlin/house/greenhouse/greenhouseconfig/gradle/Properties.kt
+++ buildSrc/src/main/kotlin/house/greenhouse/greenhouseconfig/gradle/Properties.kt
@@ -12,11 +12,11 @@ object Properties {
 
     val MODULES = mapOf(
         "core" to ModuleProperties("core", "Greenhouse Config", "greenhouseconfig", "A niche codec based config system.", "2.0.0"),
-        "jsonc" to ModuleProperties("jsonc", "Greenhouse Config - JSONC", "greenhouseconfig_jsonc", "JSONC language support for Greenhouse Config.", "1.0.0"),
-        "night-config" to ModuleProperties("night-config", "Greenhouse Config - Night Config", "greenhouseconfig_night_config", "Night Config implementation for Greenhouse Config.", "1.0.0"),
-        "toml" to ModuleProperties("toml", "Greenhouse Config - TOML", "greenhouseconfig_toml", "TOML language support for Greenhouse Config.", "1.0.0"),
-        "yaml" to ModuleProperties("yaml", "Greenhouse Config - YAML", "greenhouseconfig_yaml", "YAML language support for Greenhouse Config.", "1.0.0"),
-        "hocon" to ModuleProperties("hocon", "Greenhouse Config - HOCON", "greenhouseconfig_hocon", "HOCON language support for Greenhouse Config.", "1.0.0"),
+        "jsonc" to ModuleProperties("jsonc", "Greenhouse Config - JSONC", "greenhouseconfig_jsonc", "JSONC language support for Greenhouse Config.", "1.0.1"),
+        "night-config" to ModuleProperties("night-config", "Greenhouse Config - Night Config", "greenhouseconfig_night_config", "Night Config implementation for Greenhouse Config.", "1.0.1"),
+        "toml" to ModuleProperties("toml", "Greenhouse Config - TOML", "greenhouseconfig_toml", "TOML language support for Greenhouse Config.", "1.0.1"),
+        "yaml" to ModuleProperties("yaml", "Greenhouse Config - YAML", "greenhouseconfig_yaml", "YAML language support for Greenhouse Config.", "1.0.1"),
+        "hocon" to ModuleProperties("hocon", "Greenhouse Config - HOCON", "greenhouseconfig_hocon", "HOCON language support for Greenhouse Config.", "1.0.1"),
     )
 
     val PLATFORMS = setOf(
