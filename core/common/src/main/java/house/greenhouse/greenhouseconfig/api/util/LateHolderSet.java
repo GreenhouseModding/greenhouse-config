3de594c7a013fbdd0408a0eb16d391f3c7caa57c
Anastasia Calico <calico@merchantcalico.net>
2025-04-27 20:15:47 +1000

    chore: Optimise imports.

--- core/common/src/main/java/house/greenhouse/greenhouseconfig/api/util/LateHolderSet.java
+++ core/common/src/main/java/house/greenhouse/greenhouseconfig/api/util/LateHolderSet.java
@@ -14,7 +14,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
