93d93441058e641fe4b02ec35c8e11ddfc14a1dd
Anastasia Calico <calico@merchantcalico.net>
2025-06-01 00:01:20 +1000

    style: Don't whitespace ++i.

--- jsonc/src/main/java/house/greenhouse/greenhouseconfig/jsonc/JsonCOps.java
+++ jsonc/src/main/java/house/greenhouse/greenhouseconfig/jsonc/JsonCOps.java
@@ -373,10 +373,10 @@ public class JsonCOps implements DynamicOps<JsonCElement> {
 			JsonCObject newObject = new JsonCObject(builder.comments());
 			List<Map.Entry<String, JsonCElement>> elements = new ArrayList<>(builder.toMap().entrySet());
 
-			for (int i = Mth.ceil(elements.size() / 2.0F); i < elements.size(); ++ i) {
+			for (int i = Mth.ceil(elements.size() / 2.0F); i < elements.size(); ++i) {
 				newObject.put(elements.get(i).getKey(), elements.get(i).getValue());
 			}
-			for (int i = 0; i < Mth.ceil(elements.size() / 2.0F); ++ i) {
+			for (int i = 0; i < Mth.ceil(elements.size() / 2.0F); ++i) {
 				newObject.put(elements.get(i).getKey(), elements.get(i).getValue());
 			}
 
