62f11d84f7ffd820811969cd11432ec8c2dcee7f
Anastasia Calico <calico@merchantcalico.net>
2025-06-01 09:54:34 +1000

    chore: Bump version and update CHANGELOG.md.

--- buildSrc/src/main/kotlin/house/greenhouse/greenhouseconfig/gradle/Properties.kt
+++ buildSrc/src/main/kotlin/house/greenhouse/greenhouseconfig/gradle/Properties.kt
@@ -16,42 +16,42 @@ object Properties {
 			"Greenhouse Config",
 			"greenhouseconfig",
 			"A niche codec based config system.",
-			"2.1.0"
+			"2.2.0"
 		),
 		"jsonc" to ModuleProperties(
 			"jsonc",
 			"Greenhouse Config - JSONC",
 			"greenhouseconfig_jsonc",
 			"JSONC language support for Greenhouse Config.",
-			"1.0.2"
+			"1.1.0"
 		),
 		"night-config" to ModuleProperties(
 			"night-config",
 			"Greenhouse Config - Night Config",
 			"greenhouseconfig_night_config",
 			"Night Config implementation for Greenhouse Config.",
-			"1.0.2"
+			"1.1.0"
 		),
 		"toml" to ModuleProperties(
 			"toml",
 			"Greenhouse Config - TOML",
 			"greenhouseconfig_toml",
 			"TOML language support for Greenhouse Config.",
-			"1.0.2"
+			"1.1.0"
 		),
 		"yaml" to ModuleProperties(
 			"yaml",
 			"Greenhouse Config - YAML",
 			"greenhouseconfig_yaml",
 			"YAML language support for Greenhouse Config.",
-			"1.0.2"
+			"1.1.0"
 		),
 		"hocon" to ModuleProperties(
 			"hocon",
 			"Greenhouse Config - HOCON",
 			"greenhouseconfig_hocon",
 			"HOCON language support for Greenhouse Config.",
-			"1.0.2"
+			"1.1.0"
 		),
 	)
 
