fa4bd0b13979458f6e9c36329489b57268fb6004
Anastasia Calico <calico@merchantcalico.net>
2025-06-01 10:13:08 +1000

    chore: Bump version.

--- buildSrc/src/main/kotlin/house/greenhouse/greenhouseconfig/gradle/Properties.kt
+++ buildSrc/src/main/kotlin/house/greenhouse/greenhouseconfig/gradle/Properties.kt
@@ -16,7 +16,7 @@ object Properties {
 			"Greenhouse Config",
 			"greenhouseconfig",
 			"A niche codec based config system.",
-			"2.2.0"
+			"2.3.0"
 		),
 		"jsonc" to ModuleProperties(
 			"jsonc",
