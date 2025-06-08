489ee96a557296635a453a5d3831def4b32d08f8
Anastasia Calico <calico@merchantcalico.net>
2025-06-09 08:26:32 +1000

    fix: Move OrderCorrectedRecordCodec to the correct package.

--- core/common/src/main/java/house/greenhouse/greenhouseconfig/impl/codec/OrderCorrectedRecordCodec.java
+++ core/common/src/main/java/house/greenhouse/greenhouseconfig/impl/codec/OrderCorrectedRecordCodec.java
@@ -1,67 +1,22 @@
 package house.greenhouse.greenhouseconfig.impl.codec;
 
-import com.mojang.datafixers.util.Pair;
-import com.mojang.serialization.*;
-import net.minecraft.util.Mth;
-
-import java.util.List;
-import java.util.stream.Stream;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 
 /**
- * Accounts for a DFU bug where RecordCodecBuilder swaps the half-point at which members are encoded.
- * <p>
- * This should only ever be used on codecs with over 4 fields, which is where this bug is present.
- * @param <T> The type parameter of the RecordCodecBuilder.
+ * Moved to API package.
+ * @see house.greenhouse.greenhouseconfig.api.OrderCorrectedRecordCodec
  */
-public class OrderCorrectedRecordCodec<T> extends MapCodec<T> {
-	private final MapCodec<T> codec;
+@Deprecated(forRemoval = true)
+public class OrderCorrectedRecordCodec {
 
-	private OrderCorrectedRecordCodec(MapCodec<T> codec) {
-		this.codec = codec;
-	}
-
+	@Deprecated(forRemoval = true, since = "2.2.1+1.21.1")
 	public static <T> Codec<T> wrap(Codec<T> codec) {
-		return new OrderCorrectedRecordCodec<>(MapCodec.assumeMapUnsafe(codec)).codec();
+		return house.greenhouse.greenhouseconfig.api.OrderCorrectedRecordCodec.wrap(codec);
 	}
 
+	@Deprecated(forRemoval = true, since = "2.2.1+1.21.1")
 	public static <T> MapCodec<T> wrap(MapCodec<T> codec) {
-		return new OrderCorrectedRecordCodec<>(codec);
-	}
-
-	@Override
-	public <T1> DataResult<T> decode(DynamicOps<T1> ops, MapLike<T1> input) {
-		return codec.decode(ops, input);
-	}
-
-	@Override
-	public <T1> RecordBuilder<T1> encode(T input, DynamicOps<T1> ops, RecordBuilder<T1> prefix) {
-		return correctEncoding(
-				prefix,
-				ops.getMap(codec.encode(input, ops, ops.mapBuilder()).build(ops.empty()).getOrThrow()).getOrThrow()
-		);
-	}
-
-	@Override
-	public <T1> Stream<T1> keys(DynamicOps<T1> ops) {
-		return codec.keys(ops);
-	}
-
-	private static <T> RecordBuilder<T> correctEncoding(RecordBuilder<T> prefix, MapLike<T> newValues) {
-		if (newValues.entries().count() > 4) {
-			List<Pair<T, T>> elements = newValues.entries().toList();
-
-			for (int i = Mth.ceil(elements.size() / 2.0F); i < elements.size(); ++i) {
-				prefix.add(elements.get(i).getFirst(), elements.get(i).getSecond());
-			}
-			for (int i = 0; i < Mth.ceil(elements.size() / 2.0F); ++i) {
-				prefix.add(elements.get(i).getFirst(), elements.get(i).getSecond());
-			}
-		} else {
-			for (Pair<T, T> entry : newValues.entries().toList()) {
-				prefix.add(entry.getFirst(), entry.getSecond());
-			}
-		}
-
-		return prefix;
+		return house.greenhouse.greenhouseconfig.api.OrderCorrectedRecordCodec.wrap(codec);
 	}
 }
