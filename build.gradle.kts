63b5cef71d699ac4e2f73c39986605578e965e3d
Anastasia Calico <calico@merchantcalico.net>
2025-08-19 12:39:12 +1000

    build: Update Fabric Loom and Mod DeV Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
-    id("fabric-loom") version "1.10-SNAPSHOT" apply false
-    id("net.neoforged.moddev") version "2.0.82" apply false
+    id("fabric-loom") version "1.11-SNAPSHOT" apply false
+    id("net.neoforged.moddev") version "2.0.107" apply false
     id("me.modmuss50.mod-publish-plugin") version "0.8.4" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
